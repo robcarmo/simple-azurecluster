dcf3914866357865bd75c2bd720d1870a57003c6
Roberto Carmo <robcarmof@gmail.com>
2024-06-17 19:36:57 -0300

    improving app endpoints for monitoring

--- docker/api/main.py
+++ docker/api/main.py
@@ -1,12 +1,19 @@
-from flask import Flask
-import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = os.getenv("NAME", "World")
-    return f'Hello, {name}!'
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "UP"}), 200
+
+@app.route('/resource', methods=['GET'])
+def get_resource():
+    # Sample data
+    data = {
+        "expected_key": "This is a sample response"
+    }
+    return jsonify(data), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
