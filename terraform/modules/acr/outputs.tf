8ffee8d9d73cb6cf54a34565ea3dbfd5fec69c72
robcarmo <33102681+robcarmo@users.noreply.github.com>
2025-04-01 15:24:21 -0300

    Implement Azure AD authentication for ACR access
    Implement Azure AD authentication for ACR access

--- terraform/modules/acr/outputs.tf
+++ terraform/modules/acr/outputs.tf
@@ -1,15 +1,3 @@
-output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-}
-
 output "acr_id" {
   value = azurerm_container_registry.acr.id
 }
