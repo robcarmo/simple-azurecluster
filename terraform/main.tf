6a3c95d4ce78aafe22cea83426992ec8559dd186
Roberto <robcarmof@gmail.com>
2025-05-25 14:59:34 -0300

    fixing code

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
+# resource "azurerm_resource_group" "main" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
 
 module "acr" {
   source = "./modules/acr"
