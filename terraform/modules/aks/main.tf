18d892a9d9471631d02551952d2209dfc255fa03
Roberto <robcarmof@gmail.com>
2025-05-25 14:28:27 -0300

    fixing code

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -6,27 +6,19 @@ resource "azurerm_kubernetes_cluster" "main" {
   kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
-    # os_disk_size_gb = 30 # Optional: specify OS disk size
+    name                = "default"
+    vm_size             = var.node_vm_size
     enable_auto_scaling = var.enable_auto_scaling
-    min_count           = var.min_node_count
-    max_count           = var.max_node_count
+    node_count          = var.enable_auto_scaling ? null : var.node_count
+    min_count           = var.enable_auto_scaling ? var.min_node_count : null
+    max_count           = var.enable_auto_scaling ? var.max_node_count : null
+    # os_disk_size_gb = 30 # Optional: specify OS disk size
   }
 
   identity {
     type = "SystemAssigned"
   }
-
-  # Optional: Network Profile (e.g., for Azure CNI)
-  # network_profile {
-  #   network_plugin    = "azure"
-  #   service_cidr      = "10.0.0.0/16"
-  #   dns_service_ip    = "10.0.0.10"
-  #   docker_bridge_cidr = "172.17.0.1/16"
-  # }
-
+      
   tags = {
     environment = "sample_app_env"
     project     = "Sample Nodejs App"
