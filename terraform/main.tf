97a999eade875b9fde0749fcf99f741c642f7b05
Roberto <robcarmof@gmail.com>
2025-05-25 15:25:15 -0300

    fixing code again

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,24 +11,25 @@ provider "azurerm" {
   features {}
 }
 
-# resource "azurerm_resource_group" "main" {
-#   name     = var.resource_group_name
-#   location = var.location
-# }
+# main.tf (or data.tf)
+
+data "azurerm_resource_group" "existing_rg" { # Changed the local name to avoid confusion
+  name = "cc-rg-new" # The exact name of your existing resource group
+}
 
 module "acr" {
   source = "./modules/acr"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   acr_name            = "acrsample${random_id.suffix.hex}" # Example name, consider a better naming convention
 }
 
 module "aks" {
   source = "./modules/aks"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   aks_cluster_name    = "aks-sample-cluster" # Example name
   kubernetes_version  = "1.27.7"             # Example version
   node_vm_size        = "Standard_DS2_v2"
@@ -38,8 +39,8 @@ module "aks" {
 module "database" {
   source = "./modules/database"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   postgresql_server_name = "psql-${var.resource_group_name}" # Example name
   postgresql_admin_login    = "psqladmin"
   postgresql_admin_password = "Password123!" # Replace with a secure password, ideally from a variable or secret
@@ -49,8 +50,8 @@ module "database" {
 module "storage" {
   source = "./modules/storage"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   storage_account_name = "stsample${random_id.suffix.hex}" # Example name
   # Add other necessary variables
 }
@@ -59,24 +60,24 @@ module "storage" {
 module "app_insights" {
   source = "./modules/app-insights"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   app_insights_name   = "appi-${var.resource_group_name}"
 }
 
 module "networking" {
   source = "./modules/networking"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   vnet_name           = "vnet-sample"
 }
 
 module "log_analytics" {
   source = "./modules/log-analytics"
 
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  location            = data.azurerm_resource_group.existing_rg.location
   log_analytics_workspace_name = "logws-${var.resource_group_name}"
 }
 
